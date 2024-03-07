a30f75ce5544ceb4973677e0b4c6a092195999ca
lukasschulte03 <lukas007@live.se>
2024-03-07 23:30:25 +0100

    Initial commit

---
+++ Extension/content-script.js
@@ -0,0 +1,83 @@
+console.log("Hello from content-script.js");
+
+class Data {
+    tabs;
+    chosen_study_area;
+    hint_element;
+    list_items;
+}
+var data = new Data();
+main();
+ 
+
+
+function main(){
+    get_user_study_area();
+    setup();
+    addEventListener("keyup", event => {
+        if (event.code === 'Space') {
+            display_random_item();
+        }
+    });
+}
+
+function get_user_study_area(){
+    chosen = ""
+    valid_input = false;
+    while (valid_input == false){
+        inp = prompt("What do you want to study? OSOF(1), Neuro(2), BV3(3)");
+        if (Number(inp) == 1) {
+            chosen = "OSOF";
+            valid_input = true;
+        }
+        else if (Number(inp) == 2) {
+            chosen = "Neuro";
+            valid_input = true;
+        }
+        else if (Number(inp) == 3) {
+            chosen = "Basvetenskap3";
+            valid_input = true;
+        }
+        else {
+            console.log("Invalid selection! Please type 1, 2 or 3");
+        }
+    }
+    data.chosen_study_area = chosen;
+}
+
+function setup(){
+    button_container = document.querySelector(".openseadragon-container");
+    console.log(button_container);
+    //get second child of button_container
+    button_container = button_container.children[1];
+    button_container = button_container.children[0];
+    button_container = button_container.children[0];
+
+    console.log(button_container);
+
+    //add an element to the button_container
+    var hint_element = document.createElement("div");
+    hint_element.className = "hint_element";
+    hint_element.innerHTML = "press 'space' for a random picture!";
+    button_container.appendChild(hint_element);
+
+    var hint_element_block = document.createElement("div");
+    hint_element_block.className = "hint_element_block";
+    hint_element.appendChild(hint_element_block);
+
+    list_items = document.querySelectorAll("#" + data.chosen_study_area + " .dropdown-content ul li");
+    data.hint_element = hint_element;
+    data.list_items = list_items;
+}
+
+function display_random_item(){
+    var idx = Math.floor(Math.random() * data.list_items.length);
+    list_items[idx].click();
+    var chosen_item_name = data.list_items[idx].innerHTML.split(".").pop();
+    data.hint_element.innerHTML = chosen_item_name;
+    
+    var hint_element_block = document.createElement("div");
+    hint_element_block.className = "hint_element_block";
+    hint_element_block.onhov
+    data.hint_element.appendChild(hint_element_block);
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Microscopy-KI-Extension
 An extension for http://mikroskop.neuro.ki.se/ to more effectively study histology
+## Installation
+* Download the code from this repository, and locate the **Extension** folder
+* Open Google Chrome and go to the menu (three dots in the top right corner) -> More tools -> Extensions (or type `chrome://extensions/` in the address bar)
+* Enable **Developer mode** in the top right corner
+* Click **Load unpacked** and select the **Extension** folder
+* The extension should now be installed and ready to use!
+## Usage
+* Go to `http://mikroskop.neuro.ki.se/` (or click the link in the extension popup)
+* Input what area you want to study in the propmt that appears when entering the site
+* Press **Space** to get a new random image from your selected area of interest
+* You can also hover your mouse over the gray box in the top left corner to see what the name of the current image is
